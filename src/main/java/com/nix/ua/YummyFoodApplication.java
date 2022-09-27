d968023af070995a41df4f817830f82b976a800e
max-x444 <maxfv1999@gmail.com>
2022-09-27 16:53:46 +0300

    First commit

---
+++ src/main/java/com/nix/ua/YummyFoodApplication.java
@@ -0,0 +1,11 @@
+package com.nix.ua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyFoodApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(YummyFoodApplication.class, args);
+	}
+}
