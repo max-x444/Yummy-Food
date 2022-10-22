26ea374d935462585d12b49e866017edf5eb54b7
max-x444 <maxfv1999@gmail.com>
2022-10-22 19:45:57 +0300

    Added security

---
+++ src/main/java/com/nix/ua/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.nix.ua.config;
+
+import com.nix.ua.handler.URLAuthenticationSuccessHandler;
+import com.nix.ua.handler.URLLogoutSuccessHandler;
+import com.nix.ua.model.enums.Role;
+import com.nix.ua.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/**", "/static/**").permitAll()
+//                .antMatchers("api/booking/get-all/admin").hasAuthority(Role.ADMIN.name())
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .successHandler(myAuthenticationSuccessHandler())
+                .permitAll()
+                .and()
+//                .rememberMe()
+//                .and()
+                .logout()
+                .permitAll()
+                .invalidateHttpSession(true)
+                .logoutUrl("/logout")
+                .logoutSuccessHandler(myLogoutSuccessHandler());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
+        return URLAuthenticationSuccessHandler.getInstance();
+    }
+
+    @Bean
+    public LogoutSuccessHandler myLogoutSuccessHandler(){
+        return URLLogoutSuccessHandler.getInstance();
+    }
+}
