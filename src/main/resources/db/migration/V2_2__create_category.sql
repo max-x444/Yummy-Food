ef1755abf61942f053e623f1eb1d690cedd61e36
max-x444 <maxfv1999@gmail.com>
2022-10-28 12:49:11 +0300

    Update link images

--- src/main/resources/db/migration/V2_2__create_category.sql
+++ src/main/resources/db/migration/V2_2__create_category.sql
@@ -1,17 +1,17 @@
 INSERT INTO "dish_category" (dish_category_id, image, name)
-VALUES ('a937102a-5aeb-4eef-8308-2e748ac0c7b1', 'https://i.ibb.co/mBxt8f1/First-course.png', 'First course');
+VALUES ('a937102a-5aeb-4eef-8308-2e748ac0c7b1', 'https://i.ibb.co/Xt6cDnd/First-course.png', 'First course');
 
 INSERT INTO "dish_category" (dish_category_id, image, name)
-VALUES ('3cc7fa36-49bc-4def-884f-cfaa2f8f967c', 'https://i.ibb.co/khyDZjt/Second-course.png', 'Second course');
+VALUES ('3cc7fa36-49bc-4def-884f-cfaa2f8f967c', 'https://i.ibb.co/X7tsgrc/Second-course.png', 'Second course');
 
 INSERT INTO "dish_category" (dish_category_id, image, name)
-VALUES ('1da065f2-d548-443c-8036-3a728ebd8aa4', 'https://i.ibb.co/HYGzT41/Garnish.png', 'Garnish');
+VALUES ('1da065f2-d548-443c-8036-3a728ebd8aa4', 'https://i.ibb.co/kJtwCQW/Garnish.png', 'Garnish');
 
 INSERT INTO "dish_category" (dish_category_id, image, name)
-VALUES ('e8b7f7c4-a9eb-4197-94f4-fae154a19b99', 'https://i.ibb.co/XXBn87n/Salad.png', 'Salad');
+VALUES ('e8b7f7c4-a9eb-4197-94f4-fae154a19b99', 'https://i.ibb.co/4R32KTg/Salad.png', 'Salad');
 
 INSERT INTO "dish_category" (dish_category_id, image, name)
-VALUES ('5bb04aaa-ce80-40df-9a5b-d78a48fd3318', 'https://i.ibb.co/xz0jSgL/Desert.png', 'Dessert');
+VALUES ('5bb04aaa-ce80-40df-9a5b-d78a48fd3318', 'https://i.ibb.co/hfvM4Fx/Desert.png', 'Dessert');
 
 INSERT INTO "dish_category" (dish_category_id, image, name)
-VALUES ('f522143b-835f-4e3b-999a-91764299fd8a', 'https://i.ibb.co/FhTf48D/Cold-drink.png', 'Cold drinks');
+VALUES ('f522143b-835f-4e3b-999a-91764299fd8a', 'https://i.ibb.co/6N3ZCzk/Cold-drink.png', 'Cold drinks');
--- src/main/resources/db/migration/V2_3__create_dish.sql
+++ src/main/resources/db/migration/V2_3__create_dish.sql
@@ -1,23 +1,23 @@
 INSERT INTO "dish" (dish_id, calories, description, image, name, price, weight, dish_category_id)
 VALUES ('70122fc4-f92c-4044-9c9e-71a01d2f0ee6', 250, 'onion, carrot, parsley, bay leaf, peppercorns, salt, beef, beets, potatoes, red bell peppers, tomato, garlic',
-    'https://i.ibb.co/KbrtyPR/Homemade-borscht-with-meat.jpg', 'Homemade borscht with meat', 50, 150, 'a937102a-5aeb-4eef-8308-2e748ac0c7b1');
+    'https://i.ibb.co/XzcqHs7/Homemade-borscht-with-meat.jpg', 'Homemade borscht with meat', 50, 150, 'a937102a-5aeb-4eef-8308-2e748ac0c7b1');
 
 INSERT INTO "dish" (dish_id, calories, description, image, name, price, weight, dish_category_id)
 VALUES ('471619bc-e4db-42db-a6ce-7c1b6ffa6e6f', 250, 'chicken breast fillet, butter, parsley, eggs, flour, salt, black pepper',
-    'https://i.ibb.co/f4X7XcS/Cutlet-is-classic-in-Kyiv-style.jpg', 'Cutlet is classic in Kyiv style', 50, 150, '3cc7fa36-49bc-4def-884f-cfaa2f8f967c');
+    'https://i.ibb.co/k4zKqGC/Cutlet-is-classic-in-Kyiv-style.jpg', 'Cutlet is classic in Kyiv style', 50, 150, '3cc7fa36-49bc-4def-884f-cfaa2f8f967c');
 
 INSERT INTO "dish" (dish_id, calories, description, image, name, price, weight, dish_category_id)
-VALUES ('97994662-abf1-4441-916f-e65db9ed9f2f', 200, '', 'https://i.ibb.co/Mp76bpx/Baked-potatoes.jpg', 'Baked potatoes',
+VALUES ('97994662-abf1-4441-916f-e65db9ed9f2f', 200, '', 'https://i.ibb.co/1RsFB6G/Baked-potatoes.jpg', 'Baked potatoes',
         45, 200, '1da065f2-d548-443c-8036-3a728ebd8aa4');
 
 INSERT INTO "dish" (dish_id, calories, description, image, name, price, weight, dish_category_id)
 VALUES ('6bffed6e-c55d-4791-b7bc-bc72f0d1687d', 100, 'arugula, Chinese cabbage, carrot, suluguni, sesame, cherry tomatoes, lime juice, olive oil, Dijon mustard',
-    'https://i.ibb.co/C2JjP78/Fresh-Greek-salad.jpg', 'Fresh Greek salad', 65, 150, 'e8b7f7c4-a9eb-4197-94f4-fae154a19b99');
+    'https://i.ibb.co/C1nVzj5/Fresh-Greek-salad.jpg', 'Fresh Greek salad', 65, 150, 'e8b7f7c4-a9eb-4197-94f4-fae154a19b99');
 
 INSERT INTO "dish" (dish_id, calories, description, image, name, price, weight, dish_category_id)
 VALUES ('6fde0896-bb42-4d0d-be19-a0d0a8c3c14c', 300, 'savoiardi cookies, espresso coffee, cocoa powder, Mascarpone cream cheese, chicken eggs',
-        'https://i.ibb.co/12R82RQ/Tiramisu-cake.jpg', 'Tiramisu cake', 150, 250, '5bb04aaa-ce80-40df-9a5b-d78a48fd3318');
+        'https://i.ibb.co/qMtKh2b/Tiramisu-cake.jpg', 'Tiramisu cake', 150, 250, '5bb04aaa-ce80-40df-9a5b-d78a48fd3318');
 
 INSERT INTO "dish" (dish_id, calories, description, image, name, price, weight, dish_category_id)
 VALUES ('0e6c2a5d-7dce-44c5-9e23-349c8138ab07', 100, '',
-        'https://i.ibb.co/qDZNC2R/Pepsi.png', 'Pepsi', 32, 500, 'f522143b-835f-4e3b-999a-91764299fd8a');
+        'https://i.ibb.co/JHVr6Lz/Pepsi.png', 'Pepsi', 32, 500, 'f522143b-835f-4e3b-999a-91764299fd8a');
